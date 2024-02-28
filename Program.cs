a9f1eb0476e032b91eab3b1c32947abcc0930838
Scotch <blackscotch@mail.ru>
2024-02-28 10:17:00 +0300

    комит для изменения и теста

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ while (true)
     {
         Console.WriteLine("Элемент найден. Индекс элемента со значением {0} равен {1}", k, search);
     }
+    // Ккомментарий для теста
 }
 
 static int [] CreateArr(){
